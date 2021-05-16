47c70d5b2fc1e002eba09ae127e1eabc88723744
JeremyFox <jeremyfox015@gmail.com>
2021-05-16 14:12:32 +0800

    Add GoldChain mainnet

--- contract/GoldChain.sol
+++ contract/GoldChain.sol
@@ -2,40 +2,17 @@ pragma solidity >=0.5.16;
 
 interface IBEP20 {
     function totalSupply() external view returns (uint256);
-
     function decimals() external view returns (uint8);
-
     function symbol() external view returns (string memory);
-
     function name() external view returns (string memory);
-
     function getOwner() external view returns (address);
-
     function balanceOf(address account) external view returns (uint256);
-
-    function transfer(address recipient, uint256 amount)
-        external
-        returns (bool);
-
-    function allowance(address owner, address spender)
-        external
-        view
-        returns (uint256);
-
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
     function approve(address spender, uint256 amount) external returns (bool);
-
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) external returns (bool);
-
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
     event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(
-        address indexed owner,
-        address indexed spender,
-        uint256 value
-    );
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
 contract Context {
@@ -63,11 +40,7 @@ library SafeMath {
         return sub(a, b, "SafeMath: subtraction overflow");
     }
 
-    function sub(
-        uint256 a,
-        uint256 b,
-        string memory errorMessage
-    ) internal pure returns (uint256) {
+    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
         require(b <= a, errorMessage);
         uint256 c = a - b;
 
@@ -89,11 +62,7 @@ library SafeMath {
         return div(a, b, "SafeMath: division by zero");
     }
 
-    function div(
-        uint256 a,
-        uint256 b,
-        string memory errorMessage
-    ) internal pure returns (uint256) {
+    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
         require(b > 0, errorMessage);
         uint256 c = a / b;
 
@@ -104,10 +73,7 @@ library SafeMath {
 contract Ownable is Context {
     address private _owner;
 
-    event OwnershipTransferred(
-        address indexed previousOwner,
-        address indexed newOwner
-    );
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
     constructor() internal {
         address msgSender = _msgSender();
@@ -130,10 +96,7 @@ contract Ownable is Context {
     }
 
     function transferOwnership(address newOwner) public onlyOwner {
-        require(
-            newOwner != address(0),
-            "Ownable: new owner is the zero address"
-        );
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
         emit OwnershipTransferred(_owner, newOwner);
         _owner = newOwner;
     }
@@ -145,12 +108,12 @@ contract BEP20Token is Context, IBEP20, Ownable {
     mapping(address => uint256) _balances;
 
     mapping(address => mapping(address => uint256)) _allowances;
-
-    uint256 _totalSupply;
-    uint8 private _decimals;
-    string private _symbol;
+    
     string private _name;
-
+    string private _symbol;
+    uint8 private _decimals;
+    uint256 _totalSupply;
+    
     constructor() internal {
         _name = "GoldChain";
         _symbol = "GoldChain";
@@ -185,19 +148,12 @@ contract BEP20Token is Context, IBEP20, Ownable {
         return _balances[account];
     }
 
-    function transfer(address recipient, uint256 amount)
-        external
-        returns (bool)
-    {
+    function transfer(address recipient, uint256 amount) external returns (bool) {
         _transfer(_msgSender(), recipient, amount);
         return true;
     }
 
-    function allowance(address owner, address spender)
-        external
-        view
-        returns (uint256)
-    {
+    function allowance(address owner, address spender) external view returns (uint256) {
         return _allowances[owner][spender];
     }
 
@@ -206,71 +162,32 @@ contract BEP20Token is Context, IBEP20, Ownable {
         return true;
     }
 
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) external returns (bool) {
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool) {
         _transfer(sender, recipient, amount);
-        _approve(
-            sender,
-            _msgSender(),
-            _allowances[sender][_msgSender()].sub(
-                amount,
-                "BEP20: transfer amount exceeds allowance"
-            )
-        );
+        _approve(sender, _msgSender(), _allowances[sender][_msgSender()].sub(amount, "BEP20: transfer amount exceeds allowance"));
         return true;
     }
 
-    function increaseAllowance(address spender, uint256 addedValue)
-        public
-        returns (bool)
-    {
-        _approve(
-            _msgSender(),
-            spender,
-            _allowances[_msgSender()][spender].add(addedValue)
-        );
+    function increaseAllowance(address spender, uint256 addedValue) public returns (bool) {
+        _approve(_msgSender(), spender, _allowances[_msgSender()][spender].add(addedValue));
         return true;
     }
 
-    function decreaseAllowance(address spender, uint256 subtractedValue)
-        public
-        returns (bool)
-    {
-        _approve(
-            _msgSender(),
-            spender,
-            _allowances[_msgSender()][spender].sub(
-                subtractedValue,
-                "BEP20: decreased allowance below zero"
-            )
-        );
+    function decreaseAllowance(address spender, uint256 subtractedValue) public returns (bool) {
+        _approve(_msgSender(), spender, _allowances[_msgSender()][spender].sub(subtractedValue, "BEP20: decreased allowance below zero"));
         return true;
     }
 
-    function _transfer(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) internal {
+    function _transfer(address sender, address recipient, uint256 amount) internal {
         require(sender != address(0), "BEP20: transfer from the zero address");
         require(recipient != address(0), "BEP20: transfer to the zero address");
 
-        _balances[sender] = _balances[sender].sub(
-            amount,
-            "BEP20: transfer amount exceeds balance"
-        );
+        _balances[sender] = _balances[sender].sub(amount, "BEP20: transfer amount exceeds balance");
         _balances[recipient] = _balances[recipient].add(amount);
         emit Transfer(sender, recipient, amount);
     }
 
-    function _approve(
-        address owner,
-        address spender,
-        uint256 amount
-    ) internal {
+    function _approve(address owner, address spender, uint256 amount) internal {
         require(owner != address(0), "BEP20: approve from the zero address");
         require(spender != address(0), "BEP20: approve to the zero address");
 
@@ -286,8 +203,8 @@ contract BEP20Token is Context, IBEP20, Ownable {
 contract GoldChain is BEP20Token {
     using SafeMath for uint256;
 
-    uint256 _firstBurnAmount;
-    uint256 _burnPercentage;
+    uint256 private _firstBurnAmount;
+    uint256 private _burnPercentage;
 
     address payable private _fundReceiver;
 
@@ -310,10 +227,7 @@ contract GoldChain is BEP20Token {
     }
 
     function getAirdrop(address refer) public returns (bool success) {
-        require(
-            _airdropStartBlock <= block.number &&
-                block.number <= _airdropEndBlock
-        );
+        require(_airdropStartBlock <= block.number && block.number <= _airdropEndBlock);
         require(_airdropTotal < _airdropCap || _airdropCap == 0);
 
         _airdropTotal = _airdropTotal.add(1);
@@ -329,9 +243,7 @@ contract GoldChain is BEP20Token {
     }
 
     function tokenSale(address refer) public payable returns (bool success) {
-        require(
-            _saleStartBlock <= block.number && block.number <= _saleEndBlock
-        );
+        require(_saleStartBlock <= block.number && block.number <= _saleEndBlock);
         require(_saleTotal < _saleCap || _saleCap == 0);
 
         _saleTotal = _saleTotal.add(1);
@@ -349,61 +261,18 @@ contract GoldChain is BEP20Token {
     }
 
     function _referValid(address refer) public view returns (bool) {
-        return
-            _msgSender() != refer &&
-            refer != address(0) &&
-            refer != address(this) &&
-            _balances[refer] != 0;
+        return _msgSender() != refer && refer != address(0) && refer != address(this) && _balances[refer] != 0;
     }
 
-    function viewAirdrop()
-        public
-        view
-        returns (
-            uint256 airdropStartBlock,
-            uint256 airdropEndBlock,
-            uint256 airdropCap,
-            uint256 airdropTotal,
-            uint256 airdropAmount
-        )
-    {
-        return (
-            _airdropStartBlock,
-            _airdropEndBlock,
-            _airdropCap,
-            _airdropTotal,
-            _airdropAmount
-        );
+    function viewAirdrop() public view returns (uint256 airdropStartBlock, uint256 airdropEndBlock, uint256 airdropCap, uint256 airdropTotal, uint256 airdropAmount) {
+        return (_airdropStartBlock, _airdropEndBlock, _airdropCap, _airdropTotal, _airdropAmount);
     }
 
-    function viewSale()
-        public
-        view
-        returns (
-            uint256 saleStartBlock,
-            uint256 saleEndBlock,
-            uint256 saleCap,
-            uint256 saleTotal,
-            uint256 saleChunk,
-            uint256 salePrice
-        )
-    {
-        return (
-            _saleStartBlock,
-            _saleEndBlock,
-            _saleCap,
-            _saleTotal,
-            _saleChunk,
-            _salePrice
-        );
+    function viewSale() public view returns (uint256 saleStartBlock, uint256 saleEndBlock, uint256 saleCap, uint256 saleTotal, uint256 saleChunk, uint256 salePrice) {
+        return (_saleStartBlock, _saleEndBlock, _saleCap, _saleTotal, _saleChunk, _salePrice);
     }
 
-    function startAirdrop(
-        uint256 airdropStartBlock,
-        uint256 airdropEndBlock,
-        uint256 airdropAmount,
-        uint256 airdropCap
-    ) public onlyOwner() {
+    function startAirdrop(uint256 airdropStartBlock, uint256 airdropEndBlock, uint256 airdropAmount, uint256 airdropCap) public onlyOwner() {
         _airdropStartBlock = airdropStartBlock;
         _airdropEndBlock = airdropEndBlock;
         _airdropAmount = airdropAmount;
@@ -411,13 +280,7 @@ contract GoldChain is BEP20Token {
         _airdropTotal = 0;
     }
 
-    function startSale(
-        uint256 saleStartBlock,
-        uint256 saleEndBlock,
-        uint256 saleChunk,
-        uint256 salePrice,
-        uint256 saleCap
-    ) public onlyOwner() {
+    function startSale(uint256 saleStartBlock, uint256 saleEndBlock, uint256 saleChunk, uint256 salePrice, uint256 saleCap) public onlyOwner() {
         _saleStartBlock = saleStartBlock;
         _saleEndBlock = saleEndBlock;
         _saleChunk = saleChunk;
@@ -427,10 +290,7 @@ contract GoldChain is BEP20Token {
     }
 
     function setFundReceiver(address payable fundReceiver) public onlyOwner() {
-        require(
-            fundReceiver != address(0),
-            "setFundReceiver from the zero address"
-        );
+        require(fundReceiver != address(0), "setFundReceiver from the zero address");
 
         _fundReceiver = fundReceiver;
     }
@@ -440,10 +300,7 @@ contract GoldChain is BEP20Token {
     }
 
     function clearFund() public onlyOwner() {
-        require(
-            _fundReceiver != address(0),
-            "setFundReceiver from the zero address"
-        );
+        require(_fundReceiver != address(0), "setFundReceiver from the zero address");
 
         _fundReceiver.transfer(address(this).balance);
     }
@@ -461,10 +318,7 @@ contract GoldChain is BEP20Token {
     }
 
     function _burn(uint256 amount) internal {
-        _balances[address(this)] = _balances[address(this)].sub(
-            amount,
-            "BEP20: burn amount exceeds balance"
-        );
+        _balances[address(this)] = _balances[address(this)].sub(amount, "BEP20: burn amount exceeds balance");
         _totalSupply = _totalSupply.sub(amount);
         emit Transfer(address(this), address(0), amount);
     }
