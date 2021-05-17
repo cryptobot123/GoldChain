b9f291171fe56109359b7d196d8fa648fd106e5a
JeremyFox <jeremyfox015@gmail.com>
2021-05-17 21:42:00 +0800

    Add require Telegram and Twitter

--- src/lib/Airdrop/index.svelte
+++ src/lib/Airdrop/index.svelte
@@ -1,25 +1,44 @@
 <script>
     import { ethStore, web3, selectedAccount, connected } from "$lib/web3";
     import { contractAbi, contractAddress } from "$lib/contract-info";
+    import { URL_TELEGRAM, URL_TWITTER } from "$lib/Env.svelte";
 
     export let referralAddress;
     let transactionHash = "";
 
+    let socialSelected = "";
+    let airdropClaimed = false;
+    let telegramUsername = "";
+    let twitterUsername = "";
+    $: if (socialSelected) {
+        telegramUsername = twitterUsername = "";
+    };
+    $: validTelegram = Boolean(telegramUsername?.trim());
+    $: validTwitter = Boolean(twitterUsername?.trim());
+    $: canGetAirdrop = !airdropClaimed && (validTelegram || validTwitter)
+
     const getAirdrop = async () => {
-        if(referralAddress === "0x0000000000000000000000000000000000000000")
-        {
+        if (referralAddress === "0x0000000000000000000000000000000000000000") {
             alert(`Invited by Wallet Address is Required!`);
             return;
         }
 
         await ethStore.setBrowserProvider();
         const web3Contract = new $web3.eth.Contract(contractAbi, contractAddress);
-        const tx = await web3Contract.methods
+        const txTask = web3Contract.methods
             .getAirdrop(referralAddress)
             .send({ from: $selectedAccount })
             .catch((e) => console.log(e));
 
-        tx && (transactionHash = tx.transactionHash);
+        airdropClaimed = true;
+
+        const tx = await txTask;
+        if(!tx) {
+            airdropClaimed = false;
+            return;
+        }
+
+        transactionHash = tx.transactionHash;
     };
 </script>
 
@@ -29,7 +48,16 @@
 
 <div>
     <div class="form-group">
-        <button class={"btn btn-ico btn-block"} on:click={getAirdrop}> CLAIM 1,000,000 GOLDCHAIN </button>
+        <button class={"btn btn-ico btn-block"} type="button" on:click={() => { socialSelected = "Telegram"; window.open(`${URL_TELEGRAM}`, '_blank'); }}> Join Telegram </button>
+        <button class={"btn btn-ico btn-block"} on:click={() => { socialSelected = "Twitter"; window.open(`${URL_TWITTER}`, '_blank'); }}> Join Twitter </button>
+
+        {#if socialSelected === "Telegram"}
+            <input class={"form-control ico-form"} name="text" bind:value={telegramUsername} aria-label="Telegram Username" placeholder="Please enter your Telegram Username" />
+        {:else if socialSelected === "Twitter"}
+            <input class={"form-control ico-form"} name="text" bind:value={twitterUsername} aria-label="Twitter Username" placeholder="Please enter your Twitter Username" />
+        {/if}
+
+        <button class={"btn btn-ico btn-block"} disabled={!canGetAirdrop} on:click={getAirdrop}> CLAIM 1,000,000 GOLDCHAIN </button>
     </div>
     {#if transactionHash}
         <p>Transaction receipt: {transactionHash}</p>
--- src/lib/Env.svelte
+++ src/lib/Env.svelte
@@ -1,4 +1,6 @@
 <script context="module">
     export const WEB_DOMAIN = import.meta.env.VITE_WEB_DOMAIN || "https://goldchain.pages.dev";
     export const TOTAL_RAISED = "184,622";
+    export const URL_TELEGRAM = "https://t.me/goldchaingroup";
+    export const URL_TWITTER = "https://twitter.com/GoldChainToken";
 </script>
